7da062e5d5e383b5a270dc94dddd247636c527b4
Shevraar <sky050577@gmail.com>
2017-11-21 18:32:41 +0100

    j'ai enlevé des truc qui servaient pas

--- examples/files.js
+++ examples/files.js
@@ -1,10 +1,6 @@
 var files = {
 	"webgl": [
-		"webgl_chateau",
-		"webgl_chateau_Bowser",
-		"webgl_geometry_cube",
-		"webgl_geometry_terrain_raycast",
-		"misc_controls_orbit"
+		"webgl_chateau"
 		
 		]
 };
